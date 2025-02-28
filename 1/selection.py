604fac939ab37c0e46bec402357c18d617513cb7
Hamza Walid <hamzawalidcr@gmail.com>
2025-03-01 00:14:16 +0200

    finished the assignment

--- 1/selection.py
+++
@@ -1,9 +0,0 @@
-def selectionSort(arr, n):
-    for i in range (n-1):
-        min = i
-        j = i + 1
-        while j < n:
-            if arr[j] < arr[min]:
-                min = j
-        arr[j], arr[min] = arr[min], arr[j]
-        
