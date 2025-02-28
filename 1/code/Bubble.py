7b38f6df2f1a2038e7cea2c38610bc3ba0a34698
Hamza Walid <hamzawalidcr@gmail.com>
2025-02-28 13:33:52 +0200

    Create Bubble.py

---
+++ 1/code/Bubble.py
@@ -0,0 +1,16 @@
+import time
+def bubblesort(arr):
+    begin = time.time()
+    n = len(arr)
+    for i in range(n-1):
+        flag = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                flag = True
+            if not flag:
+                break
+
+    end = time.time()
+    total = end - begin
+    print(f"the runtime of bubbleSort is {total} seconds")
