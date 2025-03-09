254d027cc1a186b78629c27dec7120a0e644fcea
asserhanafy <asserhanafy68@gmail.com>
2025-03-09 15:58:18 +0200

    Add Merge Sort

---
+++ 1/code/merge.py
@@ -0,0 +1,34 @@
+def merge(arr, first, mid, last):
+    temp = []
+    first1 = first
+    last1 = mid
+    first2 = mid + 1
+    last2 = last
+    i = first1
+    while first1 <= last1 and first2 <= last2:
+        if arr[first1] < arr[first2]:
+            temp[i] = arr[first1]
+            first1 += 1
+        else:
+            temp[i] = arr[first2]
+            first2 += 1
+        i += 1
+    while first1 <= last1:
+        temp[i] = arr[first1]
+        first1 += 1
+        i += 1
+    while first2 <= last2:
+        temp[i] = arr[first2]
+        first2 += 1
+        i += 1
+    i = first
+    while i <= last:
+        arr[i] = temp[i]
+        i += 1
+
+def merge_sort(arr, first, last):
+    if first < last:
+        mid = (first + last) // 2
+        merge_sort(arr, first, mid)
+        merge_sort(arr, mid + 1, last)
+        merge(arr, first, mid, last)
