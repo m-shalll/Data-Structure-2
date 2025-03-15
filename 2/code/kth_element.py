5754e02ce1b1a19a3a5635ebdf771907bbb0180f
Hamza Walid <hamzawalidcr@gmail.com>
2025-03-15 06:05:56 +0200

    edited kth element algorithm

--- 2/code/kth_element.py
+++ 2/code/kth_element.py
@@ -1,8 +1,8 @@
-from quick import part
+from quick import partition
 def getKthElement(list, index, start=0, end=None):
     if end is None:
         end = len(list) - 1
-    i=part(list,start,end)
+    i=partition(list,start,end)
     if(i==index-1): print(list[i])
     elif(i>index-1):getKthElement(list,index,start,i-1)
     else: getKthElement(list,index,i+1,end)
